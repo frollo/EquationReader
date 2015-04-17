3b582d2d6ecef235d366a6b1f83cf0da1e96fc40
Lorenzo Rossi <rossi.lorenzo@yandex.com>
2015-04-17 10:38:20 +0200

    Creation of the xs' and ys' dictionaries

--- equationReader.py
+++ equationReader.py
@@ -4,9 +4,31 @@ import re
 
 if len(sys.argv) != 3:
     raise Exception("Usage: equationReader <input file> <output file>")
-fin = open(sys.argv[1], "r")
-fout = open(sys.argv[2], "w")
 
+fin = open(sys.argv[1], "r")
+
+variables = {}
+outs = {}
 for line in fin:
     if line.find("inputs" , beg=0, end=len(line)):
-        inputLine = next(fin)
+        #Creation of the x set
+        xLine = next(fin)
+        xValueLine = next(fin)
+        ins = string.split(xLine, " ")
+        insValues = string.split(xValueLine, " ")
+        if len(ins) != len(insValues):
+            raise Exception("equationReader: you need to provide a starting value for each x inserted")
+        for i in range(len(ins)):
+            tmp = int(insValues[i])
+            if (tmp != 0) & (tmp != 1):
+                raise Exception("equationReader: you need to provide binary values")
+            variables[ins[i]] = tmp
+    else if line.find("outputs", beg=0, end=len(line)):
+        #Creation of the y set
+        yLine = next(fin)
+        ins = string.split(xLine, " ")
+        for y in ins:
+            outs [y] = None
+    else if line.find("begin", beg = 0, end = len(line)):
+        #When the equations start we get to the next cicle which performs the calculations
+        break
